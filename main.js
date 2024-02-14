7f7e0684d50e2e377ef9cfdc59f7932dac20293f
LAbeast021 <kiarashhesampour@gmail.com>
2024-02-13 21:49:38 -0800

    got a working burger

---
+++ main.js
@@ -0,0 +1,6 @@
+const burgerElement = document.querySelector('.burger');
+
+burgerElement.addEventListener('click', () => {
+  const isExpanded = burgerElement.getAttribute('aria-expanded') === 'true'; 
+  burgerElement.setAttribute('aria-expanded', !isExpanded);
+});
