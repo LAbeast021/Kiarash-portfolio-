d86ace695dd90f67037e255e638cead95ba86abd
LAbeast021 <kiarashhesampour@gmail.com>
2024-02-13 23:18:15 -0800

    working on the navbar

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const burgerElement = document.querySelector('.burger');
+const menuOptionsDiv = document.querySelector('.menu-options-div');
 
 burgerElement.addEventListener('click', () => {
-  const isExpanded = burgerElement.getAttribute('aria-expanded') === 'true'; 
+  const isExpanded = burgerElement.getAttribute('aria-expanded') === 'true';
   burgerElement.setAttribute('aria-expanded', !isExpanded);
+  menuOptionsDiv.classList.toggle('show'); // Toggle visibility of the menu
 });
