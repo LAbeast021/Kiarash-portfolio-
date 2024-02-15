550703512d909f31e21523ac72c53c8d64f03f29
LAbeast021 <kiarashhesampour@gmail.com>
2024-02-15 01:07:16 -0800

    works without transittion

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const burgerElement = document.querySelector('.burger');
 const menuOptionsDiv = document.querySelector('.menu-options-div');
+const navUl = document.querySelector('.nav-ul');
 
 burgerElement.addEventListener('click', () => {
   const isExpanded = burgerElement.getAttribute('aria-expanded') === 'true';
   burgerElement.setAttribute('aria-expanded', !isExpanded);
   menuOptionsDiv.classList.toggle('show'); // Toggle visibility of the menu
+  navUl.classList.toggle('appear'); // Toggle visibility of the menu
 });
