1585f323ccc03294e00fd0df5add8eb220c8af3f
LAbeast021 <kiarashhesampour@gmail.com>
2024-02-18 04:03:47 -0800

    trying to fix the footer to go down

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+// Constanst Declarations : ----------------------
 const burgerElement = document.querySelector('.burger');
 const menuOptionsDiv = document.querySelector('.menu-options-div');
 const navUl = document.querySelector('.nav-ul');
+// ------------------------------------------------
 
 burgerElement.addEventListener('click', () => {
   const isExpanded = burgerElement.getAttribute('aria-expanded') === 'true';
